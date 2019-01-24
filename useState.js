5a24ac9908526f596a56b3b73596060286a9dfe8
Ziye Wang <zwang@LT-C02SF4R4G8WP.local>
2019-01-24 11:31:39 -0500

    simple implementation for react hooks

---
+++ useState.js
@@ -0,0 +1,26 @@
+let state;
+
+const useState = initialState => {
+  const setState = newState => (state = newState);
+
+  if (!state) {
+    state = initialState;
+  }
+
+  return [state, setState];
+};
+
+const FunctionalComponet = () => {
+  const [count, setCount] = useState(0);
+  const UI = `
+        <div>COUNT</div>
+        <div>${count}</div>`;
+  setCount(count + 1);
+  return UI;
+};
+
+const render = env => Component => env(Component());
+
+render(console.log)(FunctionalComponet);
+render(console.log)(FunctionalComponet);
+render(console.log)(FunctionalComponet);

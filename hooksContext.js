453bb54dc4597f94d97c847183617da3c40c8e0e
Ziye Wang <zwang@LT-C02SF4R4G8WP.local>
2019-02-04 01:59:35 -0500

    add statehooks

--- hooksContext.js
+++ hooksContext.js
@@ -5,19 +5,27 @@ const hooksContext = () => {
   let fnWrapper = fn => {
     const statefulFn = (...args) => {
       fnStack.push(statefulFn);
-      console.log(fnStack);
       try {
         return fn(...args);
       } finally {
         fnStack.pop();
-        console.log(fnStack);
       }
     };
 
     return statefulFn;
   };
 
-  let useState = () => {};
+  let useState = initalState => {
+    const currentFn = fnStack[fnStack.length - 1];
+    if (!stateHeap[currentFn]) {
+      const newState = {
+        nextIndex: 0,
+        state: initalState ? [initalState] : []
+      };
+    }
+    const { state, nextIndex } = stateHeap[currentFn];
+    return [state[nextIndex], () => {}];
+  };
 
   return [fnWrapper, useState];
 };
